efb4a32c094799347f7a9d01182f4f15ec757d7f
Rucheelt <narsingraosto@gmail.com>
2025-01-13 17:39:43 +0530

    operations in tuple

---
+++ operations in tuple.py
@@ -0,0 +1,13 @@
+t=("mobile", "laptop", ("apple" , "samsung"), ["pant", "shirt"])
+
+print(t)
+
+print(t)
+
+for i in (t):
+    print("phone",t)
+
+a=("M","o","b","i","l","e")
+
+for n in (t):
+    print("phone",n)
